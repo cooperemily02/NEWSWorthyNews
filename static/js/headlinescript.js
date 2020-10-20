b81d182aa28d990c2d03a3565d7b07f6f9f473ba
cooperemily02 <72948738+cooperemily02@users.noreply.github.com>
2020-10-19 20:23:12 -0400

    Update headlinescript.js

--- static/js/headlinescript.js
+++ static/js/headlinescript.js
@@ -1,9 +1,11 @@
 let li = document.createElement('li');
 let articles = document.querySelector('#articlelist')
 function submitArticleRequest(){
+    submitbutton.style.visibility = 'hidden';
+    searchbar.style.visibility = 'hidden';
     let topic = document.getElementById("searchbar").value;
     let url = 'http://newsapi.org/v2/everything?' +
-              'q=${topic}&' +
+              'q=' + topic + '&' +
               'sortBy=popularity&' +
               'apiKey=363f5cb246e3405d9b0ffc826f002954';
     fetch(url).then((res)=>{
@@ -12,18 +14,30 @@ function submitArticleRequest(){
         console.log(data)
         let i = 0;
        data.articles.forEach(article=> {
-           //createArticle(article.title, article.author, new Date(article.date), article.description, article.url, article.urltoImage);
+        //if(article.content.includes("entertainment") || article.content.includes("technology") || article.content.includes("sports"))
+        //{
+           
+            var container = document.createElement('div');
+            container.setAttribute("class", "article");
+            document.body.appendChild(container);
+
+           //Article title and link
            var a = document.createElement('a');
            a.setAttribute('href', article.url)
            a.setAttribute('target', "_blank");
            a.setAttribute('style', 'font-size:40px')
            a.innerHTML = article.title;
+           a.setAttribute("text-align", "center");
            document.body.appendChild(a);
            document.body.appendChild(document.createElement("br"));
-           var d = document.createElement('d');
+
+           //Author
+           var d = document.createElement('p');
            d.innerHTML = article.author;
            document.body.appendChild(d);
            document.body.appendChild(document.createElement("br"));
+
+           //Image
            if (article.urlToImage!=null)
            {
             var b = document.createElement("img");
@@ -31,55 +45,21 @@ function submitArticleRequest(){
             document.body.appendChild(b);
             document.body.appendChild(document.createElement("br"));
            }
-           var c = document.createElement('P');
+
+           //Description
+           var c = document.createElement('p');
            c.innerHTML = "Description: " + article.description
            document.body.appendChild(c);
-           var d = document.createElement('P');
+
+           //Date
+           var d = document.createElement('p');
            d.innerHTML = "Published: " + new Date(article.publishedAt);
            document.body.appendChild(d);
+        //}
         
-         })})
+         })
+        })
     console.log(topic);
     }
 
 
-/*function createArticle(title, author, date, description, url, urltoImage)
-{
-    let newarticle = li.createElement('li');
-
-    //title
-    var arttitle = newarticle.createElement('a');
-    arttitle.setAttribute("href", url);
-    arttitle.setAttribute("target", "_blank");
-    arttitle.textContent = title;
-    newarticle.appendChild(arttitle);
-
-    //author
-    var artauthor = newarticle.createElement('P');
-    artauthor.setAttribute("class", "tab");
-    artauthor.innerText = author;
-    newarticle.appendChild(artauthor);
-
-    //date
-    var artdate = newarticle.createElement('P');
-    artdate.setAttribute("class", "tab");
-    artauthor.innerText = new Date(date);
-    newarticle.appendChild(artadate)
-
-    //description
-    var artdescription = newarticle.createElement('P');
-    artdescription.setAttribute("class", "tab");
-    artdescription.innerText = description;
-    newarticle.appendChild(artdescription);
-
-    //image
-    var artimage = newarticle.createElement('img');
-    artimage.setAttribute("src", urltoImage);
-    newarticle.appendChild(artimage);
-
-    li.appendChild(newarticle);
-
-
-
-}
-*/
