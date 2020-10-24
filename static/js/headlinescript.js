970d3321a8f52e964407d1b54ec22d899b604ee1
cooperemily02 <72948738+cooperemily02@users.noreply.github.com>
2020-10-24 11:30:19 -0400

    Update headlinescript.js

--- static/js/headlinescript.js
+++ static/js/headlinescript.js
@@ -100,10 +100,7 @@ function submitArticleRequest(){
         location.reload()
     }
 
-    //Get the button:
 mybutton = document.getElementById("totop");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -114,10 +111,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; 
 }
 
 
